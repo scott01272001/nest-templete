49469588146e1631c712b3ec72bc4108d8415bc0
scott chiang <scott01272001@gmail.com>
2025-02-20 07:30:20 +0800

    deelete unused file

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,23 +1,33 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { UserModule } from './user/user.module';
 import configuration from './config/configuration';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
   imports: [
     AuthModule,
     ConfigModule.forRoot({
       isGlobal: true,
-      envFilePath: [`env/.env.${process.env.NODE_ENV || 'development'}`, 'env/.env'],
+      envFilePath: [
+        `env/.env.${process.env.NODE_ENV || 'development'}`,
+        'env/.env',
+      ],
       load: [configuration],
+    }),
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => ({
+        uri: configService.get<string>('MONGODB_URI'),
+      }),
+      inject: [ConfigService],
     }),
     UserModule,
   ],
-  controllers: [AppController],
+  controllers: [],
   providers: [AppService],
-  exports: [ConfigModule]
+  exports: [ConfigModule],
 })
-export class AppModule { }
+export class AppModule {}
